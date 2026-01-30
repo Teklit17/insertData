e07c5bedb8a11f5f9752a2310a1b1671157a30b7
teklit amanuel <teklitag@uia.no>
2026-01-30 18:59:55 +0100

    add mattew

---
+++ my-firestore-seed/src/data/newTestament/matthew.ts
@@ -0,0 +1,441 @@
+export const matthewBookContent = {
+  bookId: "matthew",
+
+  bookName_en: "Matthew",
+  bookName_ti: "ማቴዎስ",
+
+  theme_en: "Jesus the Messiah and King",
+  theme_ti: "ኢየሱስ እቲ መሲሕን ንጉስን",
+
+  divisions: [
+    {
+      order: 1,
+
+      title_en: "The Birth and Early Life of Jesus",
+      title_ti: "ልደትን ቀዳሞት ዓመታት ኢየሱስ",
+
+      range_en: "Matthew 1–2",
+      range_ti: "ማቴዎስ 1–2",
+
+      verse_en:
+        "She will give birth to a son, and you are to give him the name Jesus, because he will save his people from their sins.",
+      verse_ti:
+        "ወዲ ኽትወልድ እያ፡ ንሱ ኸኣ ንህዝቡ ኻብ ሓጢኣቶም ኬድሕኖም እዩ እሞ፡ ስሙ የሱስ ክትሰምዮ ኢኻ።",
+      verse_number: "1፡21",
+
+      sections: [
+        {
+          order: 1,
+          title_en: "The Genealogy of Jesus",
+          title_ti: "ወለዶ ኢየሱስ ክርስቶስ",
+          range_en: "Matthew 1:1–17",
+          range_ti: "ማቴዎስ 1፡1–17",
+          verse_en:
+            "This is the genealogy of Jesus the Messiah the son of David, the son of Abraham.",
+          verse_ti: "መጽሓፍ ወለዶ የሱስ ክርስቶስ ወዲ ዳዊት፡ ወዲ ኣብርሃም።",
+          verse_number: "1፡1",
+        },
+        {
+          order: 2,
+          title_en: "The Birth of Jesus Christ",
+          title_ti: "ልደት ኢየሱስ ክርስቶስ",
+          range_en: "Matthew 1:18–25",
+          range_ti: "ማቴዎስ 1፡18–25",
+          verse_en:
+            "This is how the birth of Jesus the Messiah came about: His mother Mary was pledged to be married to Joseph.",
+          verse_ti:
+            "ልደት የሱስ ክርስቶስ ከምዚ እዩ፡ ኣዲኡ ማርያም ንዮሴፍ ተሐጽያ ኸላ፡ ከይተራኸቡ፡ ብመንፈስ ቅዱስ ጠኒሳ ተረኽበት።",
+          verse_number: "1፡18",
+        },
+        {
+          order: 3,
+          title_en: "The Visit of the Magi",
+          title_ti: "ምምጻእ ሰብ ጥበብ",
+          range_en: "Matthew 2:1–12",
+          range_ti: "ማቴዎስ 2፡1–12",
+          verse_en:
+            "Where is the one who has been born king of the Jews? We saw his star when it rose and have come to worship him.",
+          verse_ti:
+            "እቲ እተወለደ ንጉስ ኣይሁድ ኣበይ አሎ፧ ኰዀቡ ኣብ ምብራቕ ርኢና እሞ፡ ክንሰግደሉ መጺእና፡ እናበሉ ኻብ ምብራቕ ናብ የሩሳሌም መጹ።",
+
+          verse_number: "2፡2",
+        },
+        {
+          order: 4,
+          title_en: "The Escape to Egypt",
+          title_ti: "ስደት ናብ ግብጽን ቅትለት ህጻናትን",
+          range_en: "Matthew 2:13–15",
+          range_ti: "ማቴዎስ 2፡13–15",
+          verse_en:
+            "2:13 – Get up, take the child and his mother and escape to Egypt. Stay there until I tell you.",
+          verse_ti:
+            "2:13 – ንሳቶም ምስ ሐለፉ፡ እንሆ፡ መልኣኽ እግዚኣብሄር ንዮሴፍ ብሕልሚ ተራእዮ፡ ሄሮዶስ ነቲ ሕጻን ኪቐትሎ ይደሊ አሎ እሞ፡ ተንስእ፡ ነቲ ሕጻንን ነዲኡን ሒዝካ ናብ ግብጺ ህደም፡ ክሳዕ ዝነግረካ ኸኣ ኣብኡ ጽናሕ፡ በሎ።",
+        },
+        {
+          order: 5,
+          title_en: "The Return to Nazareth",
+          title_ti: "ምምላስ ናብ ናዝሬት",
+          range_en: "Matthew 2:19–23",
+          range_ti: "ማቴዎስ 2፡19–23",
+          verse_en:
+            "And he went and lived in a town called Nazareth. So was fulfilled what was said through the prophets.",
+          verse_ti:
+            "እቲ ብነብያት፡ ናዝሬታዊ ኺበሀል፡ እተበሀለ ምእንቲ ኺፍጸም፡ ናዝሬት ናብ እትበሀል ዓዲ መጺኡ ተቐመጠ።",
+          verse_number: "2፡23",
+        },
+      ],
+    },
+    {
+      order: 2,
+
+      title_en: "The Preparation and Ministry of Jesus",
+      title_ti: "ቅድመ ምድላውን፣ ኣገልግሎትን ኢየሱስ",
+
+      range_en: "Matthew 3–4",
+      range_ti: "ማቴዎስ 3–4",
+
+      verse_en:
+        "And a voice from heaven said, “This is my Son, whom I love; with him I am well pleased.”",
+      verse_ti: "እንሆ ድማ ብእኡ ዝሰመርኩ ፍትዊ ወደይ እዚ እዩ፡ ዚብል ድምጺ ኻብ ሰማይ መጸ።",
+      verse_number: "3፡17",
+
+      sections: [
+        {
+          order: 1,
+          title_en: "The Ministry of John the Baptist",
+          title_ti: "ስብከት ዮሃንስ መጥምቅ",
+          range_en: "Matthew 3:1–12",
+          range_ti: "ማቴዎስ 3፡1–12",
+          verse_en: "Repent, for the kingdom of heaven has come near.",
+          verse_ti: "መንግስተ ሰማያት ቀሪባ ኣላ እሞ፡ ተነስሑ፡ ኢሉ እናሰበኸ፡ ናብ በረኻ ይሁዳ መጸ።",
+          verse_number: "3፡2",
+        },
+        {
+          order: 2,
+          title_en: "The Baptism of Jesus",
+          title_ti: "ጥምቀት የሱስ",
+          range_en: "Matthew 3:13–17",
+          range_ti: "ማቴዎስ 3፡13–17",
+          verse_en:
+            "As soon as Jesus was baptized, he went up out of the water.",
+          verse_ti:
+            "የሱስ ምስ ተጠምቀ፡ ብኡብኡ ኻብ ማይ ወጸ። እንሆ ኸኣ ሰማያት ተኸፍተ፡ መንፈስ ኣምላኽ ድማ ከም ርግቢ ወሪዱ፡ ኣብ ርእሱ ኪቕመጥ ረኣየ።",
+          verse_number: "3፡16",
+        },
+        {
+          order: 3,
+          title_en: "The Temptation of Jesus",
+          title_ti: "የሱስ ብድያብሎስ ከም ዝተፈተነ ",
+          range_en: "Matthew 4:1–11",
+          range_ti: "ማቴዎስ 4፡1–11",
+          verse_en:
+            "Man shall not live on bread alone, but on every word that comes from the mouth of God.",
+          verse_ti:
+            "የሱስ ግና፡ ሰብ ካብ ኣፍ ኣምላኽ ብዚወጽእ፡ ኵሉ ቓል እምበር፡ ብንጌራ ጥራይ ኣይነብርን፡ ጽሑፍ እዩ ኢሉ መለሰሉ።",
+          verse_number: "4፡4",
+        },
+        {
+          order: 4,
+          title_en: "Jesus Begins to Preach",
+          title_ti: "የሱስ ኣብ ገሊላ ክምህር ከም ዝጀመረ",
+          range_en: "Matthew 4:12–17",
+          range_ti: "ማቴዎስ 4፡12–17",
+          verse_en: "Repent, for the kingdom of heaven has come near.",
+          verse_ti:
+            "4፡17 – የሱስ ካብኡ ነጀው፡ መንግስተ ሰማይት ቀሪባ ኣላ እሞ፡ ተንስሑ፡ እናበለ ኺሰብኽ ጀመረ።",
+          verse_number: "4፡17",
+        },
+      ],
+    },
+    {
+      order: 3,
+
+      title_en: "The Sermon on the Mount",
+      title_ti: "ስብከት ከረን",
+
+      range_en: "Matthew 5–7",
+      range_ti: "ማቴዎስ 5–7",
+
+      verse_en:
+        "Everyone who hears these words of mine and puts them into practice is like a wise man who built his house on the rock.",
+      verse_ti: "ነዚ ነገረይ ሰሚዑ ዚገብሮ ኹሉ፡ ቤቱ ኣብ ከውሒ ዝሰርሔ ብልሂ ሰብኣይ ይመስል።",
+      verse_number: "7፡24",
+
+      sections: [
+        {
+          order: 1,
+          title_en: "The Beatitudes",
+          title_ti: "ትምህርቲ ብጽእና",
+          range_en: "Matthew 5:1–12",
+          range_ti: "ማቴዎስ 5፡1–12",
+          verse_en:
+            "Blessed are the poor in spirit, for theirs is the kingdom of heaven.",
+          verse_ti: "ብመንፈስ ድኻታት፡ መንግስተ ሰማያት ናቶም እያ እሞ፡ ብጹኣን እዮም።",
+          verse_number: "5፡3",
+        },
+        {
+          order: 2,
+          title_en: "Salt and Light",
+          title_ti: "ምስላ ጨውን ብርሃንን",
+          range_en: "Matthew 5:13–16",
+          range_ti: "ማቴዎስ 5፡13–16",
+          verse_en: "You are the light of the world.",
+          verse_ti: "ንስኻትኩም ብርሃን ዓለም ኢኹም። ኣብ ከረን ዘላ ኸተማ ኽትክወል ኣይከኣላን እዩ።",
+          verse_number: "5፡14",
+        },
+        {
+          order: 3,
+          title_en: "Law of Moses",
+          title_ti: "ሕጊ ሙሴ",
+          range_en: "Matthew 5:17–48",
+          range_ti: "ማቴዎስ 5፡17–48",
+          verse_en:
+            "Do not think that I have come to abolish the Law or the Prophets; I have not come to abolish them but to fulfill them.",
+          verse_ti: "ንሕጊ ዀነ ወይስ ንነብያት ክፍጽሞም እምበር፡ ክስዕሮም ዝመጻእኩ ኣይምሰልኩም።",
+          verse_number: "5፡17",
+        },
+        {
+          order: 4,
+          title_en: "Giving , Prayer, and Fasting",
+          title_ti: "ምጽዋት፣ ጸሎት፣ ጾም",
+          range_en: "Matthew 6:1–18",
+          range_ti: "ማቴዎስ 6፡1–18",
+          verse_en:
+            "Be careful not to practice your righteousness in front of others to be seen by them.",
+          verse_ti: "ጽድቅኩም ንኽትረኣዩ ኣብ ቅድሚ ሰባት ከይትገብሩ ተጠንቀቑ።",
+          verse_number: "6፡1",
+        },
+        {
+          order: 5,
+          title_en: "Treasures in Heaven",
+          title_ti: "ሰማያዊ ሃብቲ",
+          range_en: "Matthew 6:19–34",
+          range_ti: "ማቴዎስ 6፡19–34",
+          verse_en:
+            "But seek first his kingdom and his righteousness, and all these things will be given to you as well.",
+          verse_ti: "ቅድም መንግስቲ ኣምላኽን ጽድቁን ድለዩ፡ እዚ ኹሉ ድማ ይውሰኸልኩም።",
+          verse_number: "6፡33",
+        },
+        {
+          order: 6,
+          title_en: "Do Not Judge Others",
+          title_ti: "ኣይትፍረዱ",
+          range_en: "Matthew 7:1–5",
+          range_ti: "ማቴዎስ 7፡1–5",
+          verse_en: "Do not judge, or you too will be judged.",
+          verse_ti:
+            "ብዝፈረድኩምሉ ፍርዲ ኺፍረደኩም፡ ብዝሰፈርኩምሉ መስፈርውን ኪስፈረልኩም እዩ እሞ ከይፍረደኩም ኣይትፍረዱ።",
+          verse_number: "7፡1",
+        },
+        {
+          order: 7,
+          title_en: "Ask, Seek, Knock",
+          title_ti: "ለምኑ፣ ድለዩ፣ ኳሕኲሑ",
+          range_en: "Matthew 7:7–11",
+          range_ti: "ማቴዎስ 7፡7–11",
+          verse_en:
+            "Ask and it will be given to you; seek and you will find; knock and the door will be opened to you.",
+          verse_ti:
+            "ዝለመነ ኹሉ ይቕበል እዩ፡ ዝደለየ ድማ ይረክብ፡ ማዕጾ ዃሕዃሕ ንዘበለውን ይኽፈተሉ እዩ እሞ፡ ለምኑ ይውሀበኩም። ድለዩ፡ ትረኽቡ። ማዕጾ ዃሕኳሕ ኣብሉ፡ ይኽፈተልኩም እዩ።",
+          verse_number: "7፡7",
+        },
+        {
+          order: 8,
+          title_en: "The Narrow and Wide Gates",
+          title_ti: "ጸባብን ገፋሕን ኣፍደገ",
+          range_en: "Matthew 7:13–14",
+          range_ti: "ማቴዎስ 7፡13–14",
+          verse_en: "Small is the gate and narrow the road that leads to life.",
+          verse_ti:
+            "እታ ናብ ጥፍኣት እትወስድ ደገ ርሕብቲ፡ መገዳውን ገፋሕ እያ፡ እቶም ብእኣ ዚኣትዉ ድማ ብዙሓት እዮም። እታ ናብ ህይወት እትወስድ ደገ ግና ጸባብ፡ መገዳውን ቀጣን እያ፡ እቶም ዚረኽብዋ ኸኣ ሒደት እዩም እሞ፡ በታ ጸባብ ደገ",
+
+          verse_number: "7፡14",
+        },
+        {
+          order: 9,
+          title_en: "A Tree and Its Fruit",
+          title_ti: "ኦም ብፍሪኡ",
+          range_en: "Matthew 7:15–20",
+          range_ti: "ማቴዎስ 7፡15–20",
+          verse_en:
+            "Likewise, every good tree bears good fruit, but a bad tree bears bad fruit.",
+          verse_ti: "ከምኡኸስ እቲ ጽቡቕ ኦም ጽቡቕ ፍረ ይፈሪ፡ እቲ ኽፉእ ኦም ከኣ ክፉእ ፍረ ይፈሪ።",
+          verse_number: "7፡17",
+        },
+        {
+          order: 10,
+          title_en: "The Wise and Foolish Builders",
+          title_ti: "ጠቢብን ሰነፍን ሃናጺ",
+          range_en: "Matthew 7:24–27",
+          range_ti: "ማቴዎስ 7፡24–27",
+          verse_en:
+            "Everyone who hears these words of mine and puts them into practice is like a wise man who built his house on the rock.",
+          verse_ti: "ነዚ ነገረይ ሰሚዑ ዚገብሮ ኹሉ፡ ቤቱ ኣብ ከውሒ ዝሰርሔ ብልሂ ሰብኣይ ይመስል።",
+          verse_number: "7፡24",
+        },
+      ],
+    },
+    {
+      order: 4,
+
+      title_en: "The Authority and Miracles of Jesus",
+      title_ti: "ስልጣንን ተኣምራትን ኢየሱስ",
+
+      range_en: "Matthew 8–9",
+      range_ti: "ማቴዎስ 8–9",
+
+      verse_en:
+        "Jesus went through all the towns and villages, teaching, preaching, and healing every disease.",
+      verse_ti: "ኢየሱስ ኣብ ኵለን ከተማታትን መንደራትን ዞረ፡ እናምሃረ፡ እናሰበኸ፡ ኵሉ ሕማም እናፈወሰ።",
+      verse_number: "9፡35",
+
+      sections: [
+        {
+          order: 1,
+          title_en: "Jesus Heals the Sick",
+          title_ti: "ሕሙማት ተፈወሱ",
+          range_en: "Matthew 8:1–17",
+          range_ti: "ማቴዎስ 8፡1–17",
+          verse_en:
+            "Jesus reached out his hand and touched the man. “I am willing,” he said. “Be clean!”",
+          verse_ti: "ኢዱ ዘርጊሑ፡ ፈትየ አሎኹ ንጻህ፡ ኢሉ ተንከዮ። ብኡብኡ ኻብ ለምጹ ነጽሄ።",
+          verse_number: "8፡3",
+        },
+        {
+          order: 2,
+          title_en: "Jesus Calms the Storm",
+          title_ti: "የሱስ ማዕበል ኣህደአ",
+          range_en: "Matthew 8:23–27",
+          range_ti: "ማቴዎስ 8፡23–27",
+          verse_en:
+            "Then he got up and rebuked the winds and the waves, and it was completely calm.",
+          verse_ti:
+            "ንሱ ኸኣ፡ ኣቱም ጕዱላት እምነት፡ ከመይከ እትፈርሁ፧ በሎም። ሽዑ ተንሲኡ ንንፋስ ንባሕርን ገንሖም። ዓብዪ ህድኣት ከኣ ዀነ።",
+          verse_number: "8፡26",
+        },
+        {
+          order: 3,
+          title_en: "The Calling of Matthew",
+          title_ti: "ጽውዓ ማቴዎስ",
+          range_en: "Matthew 9:9–13",
+          range_ti: "ማቴዎስ 9፡9–13",
+          verse_en:
+            "Follow me,” he told him, and Matthew got up and followed him.",
+          verse_ti:
+            "የሱስ ካብኡ ምስ ሐለፈ፡ ማቴዎስ ዚብልዎ ሰብኣይ ኣብቲ በሪ ተጎ ተቐሚጡ ረኣየ እሞ፡ ስዐበኒ፡ በሎ። ተንሲኡ ኸኣ ሰዐቦ።",
+          verse_number: "9፡9",
+        },
+        {
+          order: 4,
+          title_en: "Jesus Raises the Dead",
+          title_ti: "ምዉት ተንስአ",
+          range_en: "Matthew 9:18–26",
+          range_ti: "ማቴዎስ 9፡18–26",
+          verse_en: "He took her by the hand, and the girl got up.",
+          verse_ti: "እቶም ሰብ ምስ ወጹ፡ ኣትዩ ኢዳ ሐዛ፡ እታ ጓል ከኣ ተንስኤት።",
+          verse_number: "9፡25",
+        },
+      ],
+    },
+
+    {
+      order: 5,
+      title_en: "The Mission and Instructions of Jesus",
+      title_ti: "መልእክትን መምርሕን የሱስ ን ደቂ መዛሙርቲ ",
+      range_en: "Matthew 10",
+      range_ti: "ማቴዎስ 10",
+      verse_en: "Freely you have received; freely give.",
+      verse_ti:
+        "ሕሙማት ኣሕውዩ፡ ምዉታት ኣተንስኡ፡ ለምጻማት ኣንጽሁ፡ ኣጋንንቲ ኣውጽኡ። ብኸምኡ ተቐበልኩም፡ ብኸምኡ ሃቡ።",
+      verse_number: "10፡8",
+    },
+    {
+      order: 6,
+
+      title_en: "Acceptance and Rejection of Jesus",
+      title_ti: "ምቅባልን ምንጻግን የሱስ",
+
+      range_en: "Matthew 11–12",
+      range_ti: "ማቴዎስ 11–12",
+
+      verse_en: "He who is not with me is against me.",
+      verse_ti: "ምሳይ ዘይኰነ መቓናቕንተይ እዩ፡ እቲ ምሳይ ኰይኑ ዘይእክብ ከኣ ይዘሩ።",
+      verse_number: "12፡30",
+    },
+    {
+      order: 7,
+
+      title_en: "The Parables of the Kingdom",
+      title_ti: "ምስላታት መንግስቲ ሰማይ",
+
+      range_en: "Matthew 13",
+      range_ti: "ማቴዎስ 13",
+
+      verse_en:
+        "The knowledge of the secrets of the kingdom of heaven has been given to you.",
+      verse_ti:
+        "መሊሱ በሎም፡ ምፍላጥ ምስጢር መንግስተ ሰማያት ንኣኻኣትኩም ተውሂቡ አሎ፡ ንኣታቶም ግና ኣይተዋህቦምን።",
+      verse_number: "13፡11",
+    },
+
+    {
+      order: 8,
+      title_en: "The Revealed Messiah",
+      title_ti: "ተገልጸ መሲሕ",
+
+      range_en: "Matthew 14–20",
+      range_ti: "ማቴዎስ 14–20",
+      verse_en:
+        "Whoever wants to become my disciple must deny themselves and take up their cross and follow me.",
+      verse_ti:
+        "ሽዑ የሱስ ንደቀ መዛሙርቱ በሎም፡ ንህይወቱ ኼድሕና ዚደሊ ኼጥፍኣ እዩ። እቲ ንህይወቱ ምእንታይ ዜጥፍኣ ኸኣ ኪረኽባ እዩ እሞ፡ ኪስዕበኒ ዚፈቱ እንተሎ፡ ህይወቱ ቐቢጹ፡ መስቀሉ ኣልዒሉ ይስዐበኒ።",
+      verse_number: "16፡24",
+    },
+    {
+      order: 8,
+      title_en: "The Revealed Messiah",
+      title_ti: "ተገልጸ መሲሕ",
+
+      range_en: "Matthew 14–20",
+      range_ti: "ማቴዎስ 14–20",
+      verse_en:
+        "Whoever wants to become my disciple must deny themselves and take up their cross and follow me.",
+      verse_ti:
+        "ሽዑ የሱስ ንደቀ መዛሙርቱ በሎም፡ ንህይወቱ ኼድሕና ዚደሊ ኼጥፍኣ እዩ። እቲ ንህይወቱ ምእንታይ ዜጥፍኣ ኸኣ ኪረኽባ እዩ እሞ፡ ኪስዕበኒ ዚፈቱ እንተሎ፡ ህይወቱ ቐቢጹ፡ መስቀሉ ኣልዒሉ ይስዐበኒ።",
+      verse_number: "16፡24",
+    },
+    {
+      order: 8,
+      title_en: "The King Enters Jerusalem and Confronts the Leaders",
+      title_ti: "ንጉስ ናብ የሩሳሌም ኣተወን መራሕቲ ገጠመን",
+      range_en: "Matthew 21–23",
+      range_ti: "ማቴዎስ 21–23",
+      verse_en: "Blessed is he who comes in the name of the Lord.",
+      verse_ti: "ብስም ጐይታ ዚመጽእ ብጹእ እዩ።",
+      verse_number: "21፡9",
+    },
+
+    {
+      order: 9,
+      title_en: "The Olivet Discourse and the End of the Age",
+      title_ti: "ስብከት ኦሊቬትን መወዳእታ ዘመንን",
+      range_en: "Matthew 24–25",
+      range_ti: "ማቴዎስ 24–25",
+      verse_en:
+        "Heaven and earth will pass away, but my words will never pass away.",
+      verse_ti: "ሰማይን ምድርን ኪሓልፉ እዮም፡ ቃላተይ ግና ኣይኪሓልፉን እዮም።",
+      verse_number: "24፡35",
+    },
+
+    {
+      order: 10,
+      title_en: "The Passion, Death, and Resurrection of Jesus",
+      title_ti: "መከራን ሞትን ትንሣኤን ኢየሱስ",
+      range_en: "Matthew 26–28",
+      range_ti: "ማቴዎስ 26–28",
+      verse_en: "He is not here; he has risen, just as he said.",
+      verse_ti: "ኣብዚ የለን፡ ከም ዝበሎ ተንሲኡ እዩ።",
+      verse_number: "28፡6",
+    },
+  ],
+};
